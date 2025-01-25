3d47ceee3fdb5d93b9d67f975154b6876cd04532
Lady Cans <frc2881@gmail.com>
2025-01-25 16:18:11 -0600

    Created Basic Code for the Hand Subsystem Students: Benita Menor: June

--- core/subsystems/hand.py
+++ core/subsystems/hand.py
@@ -1,5 +1,6 @@
 from typing import Callable
-from wpilib import SmartDashboard
+from wpilib import SmartDashboard, Solenoid
+from wpilib import PneumaticsModuleType
 from wpimath import units
 from commands2 import Subsystem, Command
 from rev import SparkMax, SparkMaxConfig, SparkBase
@@ -10,13 +11,71 @@ class HandSubsystem(Subsystem):
   def __init__(self):
     super().__init__()
     self._constants = constants.Subsystems.Hand
+
+    self._rollerMotor = SparkMax(self._constants.kRollerMotorCANId, SparkBase.MotorType.kBrushless)
+    self._sparkConfig = SparkMaxConfig()
+    (self._sparkConfig
+     .smartCurrentLimit(self._constants.kHandMotorCurrentLimit)
+     .inverted(True))
+    utils.setSparkConfig(
+      self._rollerMotor.configure(
+        self._sparkConfig,
+        SparkBase.ResetMode.kResetSafeParameters,
+        SparkBase.PersistMode.kPersistParameters
+      )
+    )
+
+    self._suctionMotor = SparkMax(self._constants.kSuctionMotorCANId, SparkBase.MotorType.kBrushless)
+    self._sparkConfig = SparkMaxConfig()
+    (self._sparkConfig
+     .smartCurrentLimit(self._constants.kSuctionMotorCurrentLimit)
+     .inverted(True))
+    utils.setSparkConfig(
+      self._suctionMotor.configure(
+        self._sparkConfig,
+        SparkBase.ResetMode.kResetSafeParameters,
+        SparkBase.PersistMode.kPersistParameters
+      )
+    )
+
+    self._suctionSolenoid = Solenoid(PneumaticsModuleType.REVPH, self._constants.kSuctionSolenoidPortId)
+
+  def runRollerCommand(self, speed: units.percent) -> Command:
+    return self.run(
+      lambda: self._rollerMotor.set(speed * self._constants.kHandInputLimit)
+    ).finallyDo(
+      lambda end: self.resetRoller()
+    ).withName("HandSubsystem:RunRoller")
+  
+  def runSuctionCommand(self) -> Command:
+    return self.run(
+      lambda: self._suctionMotor.set(self._constants.kSuctionMotorSpeed)
+    ).finallyDo(
+      lambda end: self.resetSuction()
+    ).until(
+      lambda: self._suctionMotor.getOutputCurrent() > self._constants.kSuctionMaxCurrent
+    ).withName("HandSubsystem:RunSuction")
+  
+  def releaseSuctionCommand(self) -> Command:
+    return self.run(
+      lambda: self._suctionSolenoid.set(False)
+    ).finallyDo(
+      lambda end: self.resetSolenoid()
+    ).withName("HandSubsystem:ReleaseSuction")
     
   def periodic(self) -> None:
     self._updateTelemetry()
   
-  def reset(self) -> None:
-    pass
+  def resetRoller(self) -> None:
+    self._rollerMotor.stopMotor()
+
+  def resetSuction(self) -> None:
+    self._suctionMotor.stopMotor()
+
+  def resetSolenoid(self) -> None:
+    self._suctionSolenoid.set(True)
 
   def _updateTelemetry(self) -> None:
-    # SmartDashboard.putNumber("Robot/Hand/Speed", self._motor.get())
-    pass
+    SmartDashboard.putNumber("Robot/Hand/Roller/Speed", self._rollerMotor.get())
+    SmartDashboard.putNumber("Robot/Hand/Suction/Speed", self._suctionMotor.get())
+ 
